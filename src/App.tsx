2c5fdb906d320bf9c8db754ec5db43fc1bb65d61
Sergey Ukustov <sergey.ukustov@machinomy.com>
2018-05-05 13:18:28 +0300

    Use vynos v0.1.4

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import * as React from 'react';
 import './App.css';
-import * as vynosF from 'vynos';
+import vynos from 'vynos'
+import * as Web3 from 'web3'
 
 const logo = require('./logo.svg');
-console.log(vynosF)
 class App extends React.Component {
 
-  render () {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          hello!
-        </p>
-        {vynosF.scriptTag()}
-      </div>
-    );
-  }
+    async componentDidMount () {
+        await vynos.display()
+        vynos.ready().then(wallet => {
+            let web3 = new Web3(wallet.provider)
+            web3.eth.getAccounts((err, accounts) => {
+                if (err) {
+                    throw err
+                } else {
+                    let account = accounts[0]
+                    console.log(account)
+                }
+            })
+        })
+    }
+
+    render () {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    hello!
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
