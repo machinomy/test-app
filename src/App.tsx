9aca8513537fd7e51ed86110d2425c194b95978d
Andrei Riaskov <ryaskov.work@gmail.com>
2018-05-23 16:37:47 +0300

    Update vynos.ready().then code

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,41 @@
-import * as React from 'react';
-import './App.css';
-import vynos from 'vynos'
-import * as Web3 from 'web3'
+import * as React from 'react'
+import './App.css'
+import { default as vynos, BuyProcessEvent, WalletBuyArguments, ChannelMeta, VynosBuyResponse } from 'vynos'
 
-const logo = require('./logo.svg');
+const logo = require('./logo.svg')
+
 class App extends React.Component {
+  async componentDidMount () {
+    await vynos.display()
+  }
 
-    async componentDidMount () {
-        await vynos.display()
-        vynos.ready().then(wallet => {
-            let web3 = new Web3(wallet.provider)
-            web3.eth.getAccounts((err, accounts) => {
-                if (err) {
-                    throw err
-                } else {
-                    let account = accounts[0]
-                    console.log(account)
-                }
-            })
+  render () {
+      vynos.ready().then(wallet => {
+        wallet.buyPromised('0xaa55976b1f7cdadf471b212415b9ba69b578227d', 100000000000000, 'http://127.0.0.1:3001/v1/accept', Date.now().toString())
+            .on(BuyProcessEvent.SENT_PAYMENT, (args: WalletBuyArguments) => {
+              console.log('PAYMENT WAS SENT!')
+            }).on(BuyProcessEvent.RECEIVED_TOKEN, (args: WalletBuyArguments, token: string) => {
+          console.log('Token is ' + token)
+        }).on(BuyProcessEvent.OPENING_CHANNEL_FINISHED, (args: WalletBuyArguments, channel: ChannelMeta) => {
+          console.log('Channel meta: ')
+          console.log(channel)
+        }).result.then((buyResponse: VynosBuyResponse) => {
+          console.log(buyResponse)
         })
-    }
+      })
 
-    render () {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    hello!
-                </p>
-            </div>
-        );
-    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          hello!
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
